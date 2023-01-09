55b6fdd82eae60ac19ea34f4f055b5ac00a49a31
tuannhat0111 <nhathtgcd191227@fpt.edu.vn>
2023-01-09 16:26:45 +0700

    ok

--- Program.cs
+++ Program.cs
@@ -6,10 +6,8 @@ namespace Exercise2_StudentClass
     {
         static void Main(string[] args)
         {
-
             Student student1 = new Student("Triet", 21, 9);
             Student student2 = new Student("Nhat", 21, 8.5);
-
             Console.WriteLine(student1.DisplayInformation());
             Console.WriteLine(student2.ToString());
             Console.ReadKey();
--- Student.cs
+++ Student.cs
@@ -6,29 +6,41 @@ namespace Exercise2_StudentClass
 {
     internal class Student
     {
-        public string Name { get; set; }
+        public string name;
         private int age;
         private double gpa;
+        
+        public string Name { 
+            get { return name; }
+            set { name = value; } }
 
-        public int Age { get => age; set => age = value; }
-        public double Gpa { set { if (value > 0 && value <= 4) gpa = value; } get { return gpa; } }
+        public int Age { 
+            get => age; 
+            set => age = value; 
+        }
+        public double Gpa { 
+            set { if (value > 0 && value <= 4) gpa = value; } 
+            get { return gpa; } 
+        }
 
-        public Student() { }
+        public Student() 
+        { 
+        }
         public Student(string name, int age, double gpa)
         {
-            Name = name;
+            this.name = name;
             this.age = age;
             this.gpa = gpa;
         }
 
         public string DisplayInformation()
         {
-            return $"Student has Name = {Name}, Age = {age}, GPA = {gpa}";
+            return $"Student Name = {name}, Age = {age}, GPA = {gpa}";
         }
 
         public override string? ToString()
         {
-            return $"Name = {Name}, Age = {age}, GPA = {gpa}";
+            return $"Name = {name}, Age = {age}, GPA = {gpa}";
         }
     }
 }
