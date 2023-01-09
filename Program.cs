5ab844c3b63e7e9c953d8bac3ee424f8ad6e6e9a
tuannhat0111 <nhathtgcd191227@fpt.edu.vn>
2023-01-09 16:20:46 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise2_StudentClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Student("Triet", 21, 9);
+            Student student2 = new Student("Nhat", 21, 8.5);
+
+            Console.WriteLine(student1.DisplayInformation());
+            Console.WriteLine(student2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2_StudentClass
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        private int age;
+        private double gpa;
+
+        public int Age { get => age; set => age = value; }
+        public double Gpa { set { if (value > 0 && value <= 4) gpa = value; } get { return gpa; } }
+
+        public Student() { }
+        public Student(string name, int age, double gpa)
+        {
+            Name = name;
+            this.age = age;
+            this.gpa = gpa;
+        }
+
+        public string DisplayInformation()
+        {
+            return $"Student has Name = {Name}, Age = {age}, GPA = {gpa}";
+        }
+
+        public override string? ToString()
+        {
+            return $"Name = {Name}, Age = {age}, GPA = {gpa}";
+        }
+    }
+}
